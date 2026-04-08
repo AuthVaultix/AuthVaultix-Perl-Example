e8e46f1497d61ce5ae30c8b6ca22f7581d912c1b
lufy <tecfaisal8@gmail.com>
2026-04-08 08:26:05 -0400

    Update main.pm

--- main.pm
+++ main.pm
@@ -7,10 +7,10 @@ use AuthVaultix;
 
 #  Initialize API
 AuthVaultix::Api(
-  "Teamdeveloperxd",
-  "5d36476ca4",
-  "4e1d8a87787f8af61c5462d12ee16e1f06d53fe314c78e985571db65f0007178",
-  "1.0"
+  "",
+  "",
+  "",
+  ""
 );
 
 print "\nConnecting...\n";
